e6b6d9a69d509ccb61e3153a319677f08f3a43a0
ayoub1920 <131956539+ayoub1920@users.noreply.github.com>
2026-04-25 20:40:03 +0100

    Add test properties for H2 database configuration

--- src/test/java/tn/esprit/donation/DonationApplicationTests.java
+++ src/test/java/tn/esprit/donation/DonationApplicationTests.java
@@ -2,12 +2,25 @@ package tn.esprit.donation;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+    "spring.datasource.url=jdbc:h2:mem:testdb",
+    "spring.datasource.driver-class-name=org.h2.Driver",
+    "spring.datasource.username=sa",
+    "spring.datasource.password=",
+    "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
+    "spring.jpa.hibernate.ddl-auto=create-drop",
+    "eureka.client.enabled=false",
+    "spring.cloud.discovery.enabled=false",
+    "spring.mail.host=localhost",
+    "spring.mail.port=25",
+    "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.mail.MailSenderAutoConfiguration"
+})
 class DonationApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
